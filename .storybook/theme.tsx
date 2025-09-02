a3c84c647de389272b805c5ce7e18773717ff0b3
paigauresh <paigauresh@gmail.com>
2025-09-03 01:47:14 +0530

    Chore: Add ESLint configuration and base TypeScript config

---
+++ .storybook/theme.tsx
@@ -0,0 +1,8 @@
+import { create } from '@storybook/theming';
+
+export default create({
+  base: "light",
+  brandTitle: 'Pulse Ui DS components',
+  brandImage: "pulseuibaselight.png",
+  brandTarget: '_self',
+});
